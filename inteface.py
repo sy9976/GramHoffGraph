4c3ea8067554b06ac1ac577300b327133562ef28
sy9976 <sy9976@gmail.com>
2015-04-18 20:17:13 +0200

    some event handles

--- inteface.py
+++ inteface.py
@@ -26,5 +26,23 @@ z winy architektury byłej szkoły."""
 w2 = Label(root, 
            justify=LEFT,
            padx = 10, 
+           fg='green',
            text=explanation).pack(side="left")
+def callback():
+    print "click!"
+
+b = Button(root, text="OK", pady=60, command=callback)
+b.pack()
+#b.pack()
+l =Label(root, text="Starting...")
+#l.grid()
+l.bind('<Enter>', lambda e: l.configure(text='Moved mouse inside'))
+l.bind('<Leave>', lambda e: l.configure(text='Moved mouse outside'))
+l.bind('<1>', lambda e: l.configure(text='Clicked left mouse button'))
+l.bind('<Double-1>', lambda e: l.configure(text='Double clicked'))
+l.bind('<B3-Motion>', lambda e: l.configure(text='right button drag to %d,%d' % (e.x, e.y)))
+
+c = Checkbutton(root, text='testing', padx=20)
+l.pack()
+c.pack()
 root.mainloop()
