9b4ee52a42d5905e4e6d0e172d4a1d2a0e76b77d
sy9976 <sy9976@gmail.com>
2015-04-22 21:16:30 +0200

    parameter interface

--- inteface.py
+++ inteface.py
@@ -9,10 +9,12 @@ root.title("GramHoffGraph")
 size = 400, 400
 input_frame = LabelFrame(root, text='input data')
 input_frame.grid(row=0,column=0)
-params_frame = LabelFrame(root, text='parameters')
-params_frame.grid(row=0,column=3)
+params_frame = LabelFrame(root,
+                 
+                text='parameters')
+params_frame.grid(row=0,column=1)
 output_frame = LabelFrame(root, text='output data')
-output_frame.grid(row=0,column=6)
+output_frame.grid(row=0,column=2)
 
 #INPUT
 image = Image.open('data/image.jpg')
@@ -38,17 +40,51 @@ insert_btn = Button(input_frame,
               justify=LEFT).grid(row=2, column=0)
 
 #FILTER
+def get_params():
+   print "Alfa = " + str(alpha.get()) + "  Beta = " +  str(beta.get()) + "  Filtr " + str(filter_value.get())
+
+filter_value = IntVar()
 filter_chck = Checkbutton(params_frame, 
-                text='filtr', 
+                text='filtr Hoffmanna', 
+                variable = filter_value,
                 justify=LEFT,
-                padx=20).grid(row=0, column=1, sticky=W)
+                padx=20).grid(row=0, column=0, sticky=E, columnspan=2)
 
-spinval = IntVar()
-s = Spinbox(params_frame, 
-              from_=1, 
-              to=180,
-              textvariable=spinval).grid(row=1, column=0)
+alpha = DoubleVar()
+slider = Scale(params_frame,
+    from_=1, 
+    to=180, 
+    resolution=0.1,
+    variable=alpha,
+    orient=HORIZONTAL)
+slider.set(20)
+slider.grid(row=1, column=1)
+  
+alpha_lbl = Label(params_frame, 
+           justify=LEFT,
+           padx = 10, 
+           text='kąt alfa').grid(row=1, column=0) 
 
+beta = DoubleVar()
+beta_slider = Scale(params_frame,
+    from_=1, 
+    to=180, 
+    resolution=0.1,
+    variable=beta,
+    orient=HORIZONTAL)
+beta_slider.set(20)
+beta_slider.grid(row=2, column=1)
+  
+beta_lbl = Label(params_frame, 
+           justify=LEFT,
+           padx = 10, 
+           text='kąt beta').grid(row=2, column=0) 
+           
+run = Button(params_frame,
+    text="Generuj",
+    command=get_params,
+    padx=60).grid(row=4, column=0,columnspan=2)
+     
 
 #OUTPUT
 output_display = Label(output_frame, image=data).grid(row=0, column=0)
